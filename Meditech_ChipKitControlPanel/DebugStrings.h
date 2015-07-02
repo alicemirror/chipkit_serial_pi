6dea93d0b8ef7b4d4090218bf389c3fc620c624e
Enrico Miglino <balearicdynamics@gmail.com>
2015-07-02 06:09:09 +0200

    ISSUE: added the DebugStrings.h header that was missing from the repository.

---
+++ Meditech_ChipKitControlPanel/DebugStrings.h
@@ -0,0 +1,22 @@
+/**
+  \file DebugStrings.h
+  \brief Definition of the debug strings
+  
+  To disable the strings usage undef the __DEBUG definition
+*/
+
+#undef __DEBUG
+
+#ifdef __DEBUG
+
+#define DEBG_PREFIX "[DEBUG]"
+#define DEBG_LIDOPEN  "LID OPEN"
+#define DEBG_LIDCLOSED  "LID CLOSED"
+
+#else
+
+#define DEBG_PREFIX ""
+#define DEBG_LIDOPEN  ""
+#define DEBG_LIDCLOSED  ""
+
+#endif
